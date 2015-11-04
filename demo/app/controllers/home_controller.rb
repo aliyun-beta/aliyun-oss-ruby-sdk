9f82e79bc9a47119a3879ce37bbfbfb02dc165f0
Newell Zhu <zlx.star@gmail.com>
2015-11-04 22:34:26 +0800

    Add Put Object

--- demo/app/controllers/home_controller.rb
+++ demo/app/controllers/home_controller.rb
@@ -11,4 +11,24 @@ class HomeController < ApplicationController
     buffer = service.bucket_objects.get(params[:key])
     send_data buffer, filename: params[:key]
   end
+
+  def new_put
+  end
+
+  def create_put
+    service = AliyunService.new
+    if params[:name].blank? || params[:file].blank?
+      return render action: :new_put
+    end
+
+    service.bucket_objects.create(params[:name], params[:file].read)
+    redirect_to root_path, notice: "Upload Success"
+  rescue Aliyun::Oss::RequestError => e
+    Rails.logger.error(e.inspect)
+    redirect_to root_path, alert: e.message
+  end
+
+  def new_post
+    
+  end
 end
--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  put '/download', to: 'home#download', as: :download
+  put 'download', to: 'home#download'
+
+  get 'new_put', to: 'home#new_put'
+  post 'new_put', to: 'home#create_put'
+
+  get 'new_post', to: 'home#new_post'
 end
