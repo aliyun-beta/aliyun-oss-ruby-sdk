3644e0007899ccab9e0a44b13addef26af83a0e7
Newell Zhu <zlx.star@gmail.com>
2015-11-04 23:39:28 +0800

    fix timezone bug

--- test/aliyun/struct/file_test.rb
+++ test/aliyun/struct/file_test.rb
@@ -25,9 +25,9 @@ describe Aliyun::Oss::Struct::File do
   end
 
   it '#share_link should get share link' do
-    Timecop.freeze(Time.parse('2015-11-04 21:59:00')) do
+    Timecop.freeze(Time.parse('2015-11-04 21:59:00 +0000')) do
       expected = 'http://bucket-name.oss-cn-beijing.aliyuncs.com/object-key?' \
-        "OSSAccessKeyId=#{access_key}&Expires=1446649140&Signature=k8WVHCj6NXrqUrJhwi0ugdg1Mr0="
+        "OSSAccessKeyId=#{access_key}&Expires=1446677940&Signature=4vOq8+Tnk2ZVBOWYtwu/iYEnUaM="
       assert_equal(expected, struct_object.share_link(3600))
     end
   end
